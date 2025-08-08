394f91f8945f85e1c34a5077b5ec0d91bc5d90d6
Ryota Mori <mori.ryota@tis.co.jp>
2025-08-08 13:41:28 +0900

    #43 SNS発行のためのIAMポリシーとロールアタッチメントを追加

--- example/actions/terraform/iam.tf
+++ example/actions/terraform/iam.tf
@@ -54,6 +54,28 @@ resource "aws_iam_role_policy_attachment" "actions_runner_s3_access" {
   policy_arn = aws_iam_policy.s3_access.arn
 }
 
+data "aws_iam_policy_document" "sns_publish" {
+  statement {
+    effect = "Allow"
+    
+    actions = [
+      "sns:Publish"
+    ]
+    
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "sns_publish" {
+  name   = "${local.name_prefix}-actions-runner-sns-policy"
+  policy = data.aws_iam_policy_document.sns_publish.json
+}
+
+resource "aws_iam_role_policy_attachment" "actions_runner_sns_publish" {
+  role       = aws_iam_role.actions_runner.name
+  policy_arn = aws_iam_policy.sns_publish.arn
+}
+
 resource "aws_eks_pod_identity_association" "actions_runner" {
   cluster_name    = data.terraform_remote_state.eks.outputs.eks_cluster_name
   namespace       = local.namespace
