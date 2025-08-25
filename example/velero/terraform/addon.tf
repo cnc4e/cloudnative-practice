58d1ace41fcdfadfa4098621f9c17a586a4e1b05
Ryota Mori <mori.ryota@tis.co.jp>
2025-08-25 10:51:36 +0900

    設定ファイルのコメントアウトと競合解決オプションの無効化

--- example/velero/terraform/addon.tf
+++ example/velero/terraform/addon.tf
@@ -2,7 +2,7 @@ resource "aws_eks_addon" "snapshot_controller" {
   cluster_name         = data.terraform_remote_state.eks.outputs.eks_cluster_name
   addon_name           = "snapshot-controller"
   addon_version        = "v8.3.0-eksbuild.1"
-  configuration_values = file("setting.json")
+#  configuration_values = file("setting.json")
   # resolve_conflicts_on_create = ""
-  resolve_conflicts_on_update = "OVERWRITE"
+#  resolve_conflicts_on_update = "OVERWRITE"
 }
