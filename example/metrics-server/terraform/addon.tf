115489bf1019f83495eb19eb153eea217c015dab
Ryota Mori <mori.ryota@tis.co.jp>
2025-09-04 11:45:45 +0900

    #120 metrics-server

---
+++ example/metrics-server/terraform/addon.tf
@@ -0,0 +1,36 @@
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["pods.eks.amazonaws.com"]
+#     }
+
+#     actions = [
+#       "sts:AssumeRole",
+#       "sts:TagSession"
+#     ]
+#   }
+# }
+
+# resource "aws_iam_role" "cloudwatch_observability" {
+#   name               = "${local.name_prefix}-cw-observability"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "cloudwatch_observability" {
+#   role       = aws_iam_role.cloudwatch_observability.name
+#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+# }
+
+resource "aws_eks_addon" "container_insights" {
+  cluster_name  = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  addon_name    = "metrics-server"
+  addon_version = "v0.8.0-eksbuild.2"
+
+  # pod_identity_association {
+  #   role_arn        = aws_iam_role.cloudwatch_observability.arn
+  #   service_account = "cloudwatch-agent"
+  # }
+}
