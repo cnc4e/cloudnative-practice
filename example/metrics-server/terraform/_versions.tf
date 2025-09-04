d1036a0d7878d9cd94b194ccc484ecc31280866b
mouriryouta <mori.ryota@tis.co.jp>
2025-09-04 11:46:44 +0900

    #120 metrics-server (#121)

---
+++ example/metrics-server/terraform/_versions.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.1"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "cn-practice-tfstate"
+    key            = "metrics-server.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "cn-practice-tfstate"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = local.region
+  default_tags {
+    tags = {
+      project = "cn-practice",
+      owner   = "mori"
+    }
+  }
+}
